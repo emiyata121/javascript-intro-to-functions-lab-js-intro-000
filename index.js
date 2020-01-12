aa968c17d19f7f2faec78735e29aa5caeb5eee28
beloved-format-3447 <enc.mk.89@gmail.com>
2020-01-12 00:39:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,12 +13,3 @@ function logShout(string){
 function logWhisper(string) {
      console.log(string.toLowerCase())
 }
-
-function sayHiToGrandma(string){
-  if(string.toLowerCase()===string){
-    return "I can't hear you!"
-  }
-
-  if(string.toUpperCase()===string){
-    return "YES INDEED!"
-  }
